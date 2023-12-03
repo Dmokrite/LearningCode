818b17b5e1fc01c1942997ac5db83914f5b6f190
Dmokrite <hg_greg@msn.com>
2023-12-03 19:17:23 +0100

    final add

--- Formation-back/learning JS/Pokedex/history.js
+++ Formation-back/learning JS/Pokedex/history.js
@@ -1,49 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
-// Récupération des données depuis le local storage
-const missedLog = JSON.parse(localStorage.getItem('missedPokemons')) || [];
-const successLog = JSON.parse(localStorage.getItem('caughtPokemons')) || [];
-const releasedLog = JSON.parse(localStorage.getItem('releasedPokemons')) || [];
+  // Récupération des données depuis le local storage
+  const missedLog = JSON.parse(localStorage.getItem('missedPokemons')) || [];
+  const successLog = JSON.parse(localStorage.getItem('caughtPokemons')) || [];
+  const releasedLog = JSON.parse(localStorage.getItem('releasedPokemons')) || [];
 
-// Fonction pour ajouter les Pokémon capturés à la liste HTML
-function addToCapturedList() {
-  const capturedList = document.getElementById('captured-list');
+    // Fonction pour ajouter les Pokémon capturés à la liste HTML
+  function addToCapturedList() {
+    const capturedList = document.getElementById('captured-list');
 
-  if (capturedList) {
-    successLog.forEach((entry, index) => {
-      const listItem = document.createElement('li');
-      // Crée une entrée de liste avec la date actuelle et le nom du Pokémon capturé
-      listItem.innerHTML = `Capturé le ${new Date().toLocaleString()} - ${entry.name}`;
-      capturedList.appendChild(listItem);
-    });
+    if (capturedList) {
+      successLog.forEach((entry, index) => {
+        const listItem = document.createElement('li');
+        // Crée une entrée de liste avec la date actuelle et le nom du Pokémon capturé
+        listItem.innerHTML = `Capturé le ${new Date().toLocaleString()} - ${entry.name}`;
+        capturedList.appendChild(listItem);
+      });
+    }
   }
-}
 
-// Fonction pour ajouter les Pokémon ratés à la liste HTML
-function addToMissedList() {
-  const missedList = document.getElementById('missed-list');
+    // Fonction pour ajouter les Pokémon ratés à la liste HTML
+  function addToMissedList() {
+    const missedList = document.getElementById('missed-list');
 
-  if (missedList) {
-    missedLog.forEach((entry, index) => {
-      const listItem = document.createElement('li');
-      // Crée une entrée de liste avec la date actuelle et le nom du Pokémon raté
-      listItem.innerHTML = `Raté le ${new Date().toLocaleString()} - ${entry.name}`;
-      missedList.appendChild(listItem);
-    });
+    if (missedList) {
+      missedLog.forEach((entry, index) => {
+        const listItem = document.createElement('li');
+        // Crée une entrée de liste avec la date actuelle et le nom du Pokémon raté
+        listItem.innerHTML = `Raté le ${new Date().toLocaleString()} - ${entry.name}`;
+        missedList.appendChild(listItem);
+      });
+    }
   }
-}
 
-// Fonction pour ajouter les Pokémon relâchés à la liste HTML
-function addToReleasedList() {
-  const releasedList = document.getElementById('released-list');
+  // Fonction pour ajouter les Pokémon relâchés à la liste HTML
+  function addToReleasedList() {
+    const releasedList = document.getElementById('released-list');
 
-  if (releasedList) {
-    releasedLog.forEach((entry, index) => {
-      const listItem = document.createElement('li');
-      // Crée une entrée de liste avec la date de relâchement et le nom du Pokémon relâché
-      listItem.innerHTML = `Relâché le ${entry.releaseDate} - ${entry.name}`;
-      releasedList.appendChild(listItem);
-    });
-  }
+    if (releasedList) {
+      releasedLog.forEach((entry, index) => {
+        const listItem = document.createElement('li');
+        // Crée une entrée de liste avec la date de relâchement et le nom du Pokémon relâché
+        listItem.innerHTML = `Relâché le ${entry.releaseDate} - ${entry.name}`;
+        releasedList.appendChild(listItem);
+      });
+    }
 
   // Fonction pour mettre à jour le texte des compteurs
   function updateCounterText() {
